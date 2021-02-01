0c918fac9667a7f871f326f7c99c02e722e6f440
imssyang <imssyang@gmail.com>
2021-02-01 11:23:50 +0800

    add sample“

---
+++ sample/helloworld/readfile2.js
@@ -0,0 +1,27 @@
+//
+// SERVER: node readfile2.js
+// CLIENT: curl http://localhost:8124
+//
+
+// load http module
+var http = require('http');
+var fs = require('fs');
+
+// create http server
+http.createServer(function (req, res) {
+
+    // open and read in helloworld2.js
+    fs.readFile('readfile2.js', 'utf8', function(err, data) {
+
+        res.writeHead(200, {'content-type': 'text/plain'});
+        if (err)
+            res.write('Could not find or open file for reading\n');
+        else
+            // if no error, write JS file to client
+            res.write(data);
+        res.end();
+    });
+}).listen(8124, function() { console.log('bound to port 8124'); });
+
+console.log('Server running on 8124');
+
