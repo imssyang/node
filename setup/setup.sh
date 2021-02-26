790ba8b28a76c27879600672fba7e53e7dffe919
imssyang <imssyang@gmail.com>
2021-02-26 10:09:39 +0800

    update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,29 +4,41 @@ APP=node
 HOME=/opt/$APP
 
 init() {
+  #https_proxy=http://127.0.0.1:8118
+  #npm config set registry https://registry.npm.taobao.org
+  #npm config set registry https://registry.npmjs.org
+
   if [[ ! -s /usr/local/bin/node ]]; then
+    echo "($APP) create symlink: /usr/local/bin/node --> /opt/node/bin/node"
     ln -s /opt/node/bin/node /usr/local/bin/node
   fi
 
   if [[ ! -s $HOME/bin/express ]]; then
+    echo "($APP) install express-generator"
     npm install express-generator -g
   fi
 
   if [[ ! -s $HOME/bin/nodemon ]]; then
+    echo "($APP) install nodemon"
     npm install nodemon -g
   fi
 
-  if [[ ! -s $HOME/bin/hexo-cli ]]; then
+  if [[ ! -s $HOME/bin/hexo ]]; then
+    echo "($APP) install hexo-cli"
     npm install hexo-cli -g
   fi
 
   if [[ ! -s $HOME/bin/yarn ]]; then
+    echo "($APP) install yarn"
     npm install yarn -g
 	yarn config set prefix "/opt/node/yarn"
 	yarn config set cache-folder "/opt/node/yarn/cache"
+	#yarn config set registry https://registry.npm.taobao.org
+	#yarn config set registry https://registry.yarnpkg.com
   fi
 
   if [[ ! -s $HOME/bin/wrangler ]]; then
+    echo "($APP) install wrangler"
     npm install @cloudflare/wrangler -g --unsafe-perm=true --allow-root
   fi
 }
